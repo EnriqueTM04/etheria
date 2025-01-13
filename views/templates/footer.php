fdc0e8b1d4a9aad68384fc5e365c88493acaefa7
Manuel <programadorjrsemad@gmail.com>
2025-01-13 01:06:58 -0600

    Leyenda de protección de datos personales

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -1,3 +1,6 @@
 <footer class="navbar navbar-expand-lg footer">
-        &copy; 2024 Club de Leones. Todos los derechos reservados.
+    <div style="display: flex; flex-direction: column; align-items: center;">
+        <div>&copy; 2024 Club de Leones. Todos los derechos reservados.</div>
+        <div><a href="http://data.ssp.cdmx.gob.mx/img/assets/img-pag/unidad_contacto_secretario/aviso.pdf" id="privacy-link" style="color: white">Política de Protección de Datos Personales</a></div>
+    </div>
 </footer>
